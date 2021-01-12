bb0f4fddba7496e3407a1f5c524647ba3b5b53f0
MATEUS GUILHERME ARAUJO DOS <20272572@cfp127aluno.local>
2021-01-12 11:38:14 -0300

    Segunda versão 12-01

--- conta/src/br/senai/sp/jandira/model/Conta.java
+++ conta/src/br/senai/sp/jandira/model/Conta.java
@@ -3,13 +3,23 @@ package br.senai.sp.jandira.model;
 public class Conta {
 	
 	public String tipo;
-	public double saldo;
+	private  double saldo;
 	public String numero;
 	public String titular;
 	public boolean ativa;
 	
-	public void depositar() {
+	public void depositar(double valorDeposito) {
+		if(valorDeposito < 0) {
+			System.out.println("o Valor deve ser maior que zero!");
+		}else {
+			saldo = saldo + valorDeposito;
+		}
+		saldo = saldo +valorDeposito;
 		
+	}
+	
+	public void consultarSaldo() {
+		System.out.println("saldo da conta: " + saldo);
 	}
 	
 	public void sacar() {
